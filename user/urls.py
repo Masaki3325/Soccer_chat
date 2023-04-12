f3b27eeb4223b61bfd2cd365860f44922fa954c9
Masaki3325 <masakino735@gmail.com>
2023-04-12 23:30:33 +0800

    set the urls

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 app_name = 'user'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup/', views.UserSignUpView.as_view(),name='signup'),
+    path('detail/', views.UserDetailView.as_view(),name='detail'),
+    path('login/', views.UserLoginView.as_view(),name='login'),
+    path('logout/', views.UserLogoutView.as_view(),name='logout'),
 ]
