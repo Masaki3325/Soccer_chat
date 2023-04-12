d5bb7106b6fbd790a2ec07ef942cf73e3f3b21f3
Masaki3325 <masakino735@gmail.com>
2023-04-12 23:29:57 +0800

    delete iamge

--- user/models.py
+++ user/models.py
@@ -36,7 +36,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True,verbose_name = 'メールアドレス')
     favorite_league = models.CharField(max_length=50, choices=LEAGUE_CHOICE, default='Jリーグ', verbose_name = '好きなリーグ')
     content = models.CharField(max_length=500,verbose_name = '自己紹介')
-    image = models.FileField(upload_to='media/images',verbose_name = 'プロフィール画像')
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     USERNAME_FIELD = 'email'
