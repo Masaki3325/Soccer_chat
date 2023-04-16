dd0e8a8dac8658ea2f216fc0b84e11ee4a665fba
Masaki3325 <masakino735@gmail.com>
2023-04-16 22:18:32 +0800

    set the admin

--- user/admin.py
+++ user/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
-from .forms import UserSignupForm, UserChangeForm
 from django.contrib.auth.models import Group
+
+from .forms import UserSignupForm, UserChangeForm
 
 
 class CustomUserAdmin(UserAdmin):
