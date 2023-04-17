71906c2063ab9940de842bda2d7e22301b85fbf4
Masaki3325 <masakino735@gmail.com>
2023-04-17 21:19:27 +0800

    refactor models

--- post/models.py
+++ post/models.py
@@ -21,7 +21,7 @@ class Team(models.Model):
     logo = models.ImageField(upload_to='image/', verbose_name=('エンブレム'))
     location = models.CharField(max_length=150, verbose_name=('本拠地'))
     temaname = models.CharField(max_length=50, verbose_name=('チームネーム'))
-    content = models.TextField(max_length=300, verbose_name=('チーム紹介'))
+    content = models.CharField(max_length=300, verbose_name=('チーム紹介'))
 
     def __str__(self):
         return self.temaname
@@ -29,11 +29,11 @@ class Team(models.Model):
 
 class Post(Common):
     team = models.ForeignKey(Team, on_delete=models.CASCADE, verbose_name=("チーム"))
-    content = models.TextField(max_length=140)
+    content = models.CharField(max_length=140)
 
    
 class Comment(Common):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name=("投稿"))
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=("投稿者"))
     created_at = models.DateTimeField(auto_now_add=True,  verbose_name=('投稿日'))
-    content = models.TextField(max_length=80, verbose_name=('投稿内容'))
+    content = models.CharField(max_length=80, verbose_name=('投稿内容'))
