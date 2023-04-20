fdd75438a914c6d0caea82f56733559c2f58dc76
Masaki3325 <masakino735@gmail.com>
2023-04-20 15:43:34 +0800

    add the ursl

--- post/urls.py
+++ post/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'post'
 
 urlpatterns = [
-    path('list/', views.PostListView.as_view(),name='list')
+    path('list/', views.PostListView.as_view(),name='list'),
+    path('detail/<int:pk>', views.PostDetailView.as_view(), name='detail'),
+    path('create/', views.PostCreateView.as_view(), name='create'),
+    path('delete/<int:pk>', views.PostDeleteView.as_view(), name='delete'),
 ]
